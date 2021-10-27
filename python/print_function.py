9645f2f15bed0e51fcb061866716adac04fc59bf
Jessica Sadler <jessicagsadler@gmail.com>
2021-10-27 13:17:53 +0200

    added print function test and arithmetic operators test

---
+++ python/print_function.py
@@ -0,0 +1,14 @@
+# The included code stub will read an integer, n, from STDIN.
+
+# Without using any string methods, try to print the following:
+# 123...n
+
+# Note that "..." represents the consecutive values in between.
+
+if __name__ == '__main__':
+    n = int(input())
+    start = 1
+    for x in range(start,n+1):
+        x + 1
+        print(x,end="")
+        
