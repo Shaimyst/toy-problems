aded263752fea89dd2c9d3e2fd804e7c0d596653
Jessica Sadler <jessica.sadler@scrive.com>
2024-02-19 20:09:12 +0100

    new test for division.py

--- python/division.py
+++ python/division.py
@@ -5,12 +5,22 @@
 # The second line should contain the result of float division, a / b.
 # No rounding or formatting is necessary.
 
+def division_problems(a, b):
+    int_div = int(a/b)
+    flt_div = float(a/b)
+    return int_div, flt_div
+
+def test():
+    a = 20
+    b = 10
+    int_div, flt_div = division_problems(a, b)
+    assert int_div == 2 and flt_div == 2
+
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
-    
-    int_div = int(a/b)
-    flt_div = float(a/b)
-    
+    int_div, flt_div = division_problems(a, b)
+
     print(int_div)
     print(flt_div)
+    test()
