34ed433273e596eb3ccf99abae228c6710954b5c
Jessica Sadler <jessica.sadler@scrive.com>
2024-02-23 16:57:55 +0100

    add test for hello_world

--- python/hello_world.py
+++ python/hello_world.py
@@ -1,3 +1,10 @@
 # first test, print Hello, World! using python
+def hello_world():
+    print("Hello, World!")
+    return "Hello, World!"
 
-print("Hello, World!")
+hello_world()
+
+# Test if hello_world function prints "Hello, World!"
+def test():
+    assert hello_world() == "Hello, World!"
